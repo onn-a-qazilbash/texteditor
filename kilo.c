66e39bb7615440fcd418daecd0af43ed734a2a33
Onn Agha Qazilbash <onnaqazilbash@Onns-MacBook-Pro.local>
2021-01-08 17:28:32 -0700

    configured more terminal flags

--- kilo.c
+++ kilo.c
@@ -1,7 +1,7 @@
 #include <ctype.h>   /* iscntrl() */
 #include <stdio.h>   /* printf() */
 #include <unistd.h>  /* read(), STDIN_FILENO */
-#include <termios.h> /* struct termios, tcgetattr(), tcsetattr, ECHO, TCSAFLUSH */
+#include <termios.h> /* struct termios, tcgetattr(), tcsetattr, ECHO, TCSAFLUSH, ISIG, IXON, IEXTEN, ICRNL, OPOST */
 #include <stdlib.h>  /* atexit() */
 
 struct termios original_termios;
@@ -20,7 +20,13 @@ void enableRawMode(){
     tcgetattr(STDIN_FILENO, &original_termios);
     atexit(disableRawMode);
     struct termios raw = original_termios;
-    raw.c_lflag &= ~(ECHO | ICANON);
+
+
+    raw.c_iflag &= ~(BRKINT | INPCK | ISTRIP | ICRNL | IXON);
+    raw.c_iflag &= ~(OPOST);
+    /* Set 8 bits per byte, if not already. */
+    raw.c_iflag &= (CS8);
+    raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
@@ -31,10 +37,10 @@ int main(){
     char c;
     while ( read(STDIN_FILENO, &c, 1) == 1  && c != 'q'){
         if ( iscntrl(c)  ){
-            printf("%d\n", c); /* Print ASCII code of char */
+            printf("%d\r\n", c); /* Print ASCII code of char */
         }
         else{
-            printf("%d ('%c')\n", c, c); /* Print ASCII code and actual character */
+            printf("%d ('%c')\r\n", c, c); /* Print ASCII code and actual character */
         }
     }
     return 0;
