9ad4dd4e95103a99f677797d65c3e63846b11148
Onn Agha Qazilbash <onnaqazilbash@Onns-MacBook-Pro.local>
2021-01-10 21:32:36 -0700

    added HOME/END key functionalities

--- kilo.c
+++ kilo.c
@@ -18,6 +18,8 @@ enum editorKey{
     ARROW_RIGHT,
     ARROW_UP,
     ARROW_DOWN,
+    HOME_KEY, 
+    END_KEY,
     PAGE_UP,
     PAGE_DOWN
 };
@@ -96,8 +98,12 @@ int editorReadKey(){
                 }
                 if (seq[2] == '~'){
                     switch(seq[1]){
+                        case '1': return HOME_KEY;
+                        case '4': return END_KEY;
                         case '5': return PAGE_UP;
                         case '6': return PAGE_DOWN;
+                        case '7': return HOME_KEY;
+                        case '8': return END_KEY;
                     }
                 }
             }
@@ -107,8 +113,17 @@ int editorReadKey(){
                     case 'B': return ARROW_DOWN;
                     case 'C': return ARROW_RIGHT;
                     case 'D': return ARROW_LEFT;
+                    case 'H': return HOME_KEY;
+                    case 'F': return END_KEY;
                 } 
             }
+        }
+        else if (seq[0] == 'O'){
+            switch (seq[1]){
+                case 'H': return HOME_KEY;
+                case 'F': return END_KEY;
+            }
+
         }
         return esc;
     }
@@ -264,6 +279,13 @@ void editorProcessKeypress(){
             write(STDOUT_FILENO, "\x1b[2J", 4);
             write(STDOUT_FILENO, "\x1b[H", 3);
             exit(0);
+            break;
+
+        case HOME_KEY:
+            E.cx = 0;
+            break;
+        case END_KEY:
+            E.cx = E.screencols - 1;
             break;
 
         case PAGE_UP:
