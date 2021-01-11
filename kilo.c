ad88160802f5ae59534293bdd8a951765b30aa26
Onn Agha Qazilbash <onnaqazilbash@Onns-MacBook-Pro.local>
2021-01-10 21:11:22 -0700

    added page up/down enums

--- kilo.c
+++ kilo.c
@@ -17,7 +17,9 @@ enum editorKey{
     ARROW_LEFT = 1000, 
     ARROW_RIGHT,
     ARROW_UP,
-    ARROW_DOWN
+    ARROW_DOWN,
+    PAGE_UP,
+    PAGE_DOWN
 };
 
 /* DATA */
@@ -88,17 +90,31 @@ int editorReadKey(){
             return esc;
         }
         if (seq[0] == '['){
-            switch(seq[1]){
-                case 'A': return ARROW_UP;
-                case 'B': return ARROW_DOWN;
-                case 'C': return ARROW_RIGHT;
-                case 'D': return ARROW_LEFT;
-            } 
+            if (seq[1] >= '0' && seq[1] <= '9'){
+                if (read(STDIN_FILENO, &seq[2], 1) != 1){
+                    return esc;
+                }
+                if (seq[2] == '~'){
+                    switch(seq[1]){
+                        case '5': return PAGE_UP;
+                        case '6': return PAGE_DOWN;
+                    }
+                }
+            }
+            else{
+                switch(seq[1]){
+                    case 'A': return ARROW_UP;
+                    case 'B': return ARROW_DOWN;
+                    case 'C': return ARROW_RIGHT;
+                    case 'D': return ARROW_LEFT;
+                } 
+            }
         }
         return esc;
     }
-
-    return c;
+    else{
+        return c;
+    }
 }
 
 int getCursorPosition(int * rows, int * cols){
